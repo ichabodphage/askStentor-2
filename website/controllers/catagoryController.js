e3452b36e699ef304e1317fc9f547d073718f6af
ichabodphage <40300103+ichabodphage@users.noreply.github.com>
2021-09-12 19:05:56 -0500

    Add files via upload

--- website/controllers/catagoryController.js
+++ website/controllers/catagoryController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var db = require('../DBtools.js');
 var urlTools = require('../helpfulMethods/urlTools.js');
 
@@ -47,4 +48,55 @@ exports.catagoryUpdateGet = function(req, res) {
 
 exports.catagoryUpdatePost = function(req, res) {
     res.send('NOT IMPLEMENTED: Catagory update POST');
+=======
+var db = require('../DBtools.js');
+var urlTools = require('../helpfulMethods/urlTools.js');
+
+exports.catagoryListShow = async function(req, res) {
+    var catList = await db.Catagory.findAll()
+    var termCountArr =[]
+
+    for(var i = 0; i < catList.length;i++){
+        var catTerms = await catList[i].getTerms()
+        termCountArr.push(catTerms.length)
+    }
+    res.render("categories", {catArray: catList, termCountArray: termCountArr, urlArr: urlTools.urlBuilder(catList)})
+    
+};
+
+exports.catagoryDetail = async function(req, res) {
+    var query = req.params.id.substring(1).replace("_","/")
+    var categoryToRead = await db.Catagory.findOne({where: {name: query}})
+    if(categoryToRead === null){
+    res.render("error")
+    }else{
+    var terms = await categoryToRead.getTerms()
+
+    res.render("categoryDesc",{cat: categoryToRead,termArray:terms,urlArr:urlTools.urlBuilder(terms)})
+    }
+};
+
+exports.catagoryCreateGet = function(req, res) {
+    res.send('NOT IMPLEMENTED: Catagory create GET');
+};
+
+exports.catagoryCreatePost = function(req, res) {
+    res.send('NOT IMPLEMENTED: Catagory create POST');
+};
+
+exports.catagoryDeleteGet = function(req, res) {
+    res.send('NOT IMPLEMENTED: Catagory delete GET');
+};
+
+exports.catagoryDeletePost = function(req, res) {
+    res.send('NOT IMPLEMENTED: Catagory delete POST');
+};
+
+exports.catagoryUpdateGet = function(req, res) {
+    res.send('NOT IMPLEMENTED: Catagory update GET');
+};
+
+exports.catagoryUpdatePost = function(req, res) {
+    res.send('NOT IMPLEMENTED: Catagory update POST');
+>>>>>>> 36329e306855b09fac9231c22778623294e78c38
 };
--- website/controllers/loginController.js
+++ website/controllers/loginController.js
@@ -1,6 +1,58 @@
+<<<<<<< HEAD
+var db = require('../DBtools.js');
+const hasher = require("../helpfulMethods/securityTools")
+const route = require("./route.js")
+
+exports.login = [
+    new route("/","get",async function(req, res) {
+        var sesh = req.session;
+        if(sesh.userName!== undefined){
+            var login = await db.User.findOne({ where: { username: sesh.userName } })
+                if(login !== null){
+                    res.redirect("/login/user")
+                }else{
+                    res.redirect("/")
+                }
+        }else{
+            res.render('login',{msg:""})
+        }
+    }),
+    new route("/user","get",async function(req, res) {
+        var sesh = req.session;
+        if(sesh.userName!== undefined){
+            var login = await db.User.findOne({ where: { username: sesh.userName } })
+            if(login !== null){
+                res.render('userhome',{user: login.username})
+            }else{
+                res.redirect("/")
+            }
+        }else{
+            res.redirect("/")
+        }
+
+    }),
+    new route("/","post",async function(req, res) {
+        var hashedPword = hasher.makeHash(req.body.pWord)
+        var login = await db.User.findOne({ where: { username: req.body.uName } })
+        console.log(login)
+        if(login === null ){
+          res.render('login',{msg:"error, username \""+req.body.uName +"\" not found"})
+        }else if( login.password === hashedPword){
+          var sesh = req.session;
+          sesh.userName = req.body.uName
+          console.log(req.session)
+          res.redirect("/login/user")
+
+        }else{
+          res.render('login',{msg:"error, incorrect password"})
+        }
+    })
+]
+=======
 var db = require('../DBtools.js');
 
 exports.userLogin = async function(req, res) {
     res.send("login page for askstentor")
     
 };
+>>>>>>> 36329e306855b09fac9231c22778623294e78c38
